a2bcf1b91e2392393720511075c634ca1a4f5eb5
Leo Bevelacqua <Leo.Bevelacqua@arrowdigital.com>
2018-03-03 03:32:51 -0500

    Adding AssetEditor project, Edit command

---
+++ src/Feature/DigitalBrewery.Feature.AssetEditor/Commands/EditAsset.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using System.Linq;
+
+namespace DigitalBrewery.Feature.AssetEditor.Commands
+{
+    public class EditAsset : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Item item = context.Items[0];
+            //Sitecore.Context.ClientPage.Start(this, "Edit", context.Parameters);
+        }
+
+        private void Edit(ClientPipelineArgs args)
+        {
+            if (args != null && args.Parameters != null && !string.IsNullOrEmpty(args.Parameters["id"]))
+            {
+                SheerResponse.Alert(args.Parameters["id"]);
+            }
+        }
+
+        private void Editor(Item item)
+        {
+            
+        }
+    }
+}
